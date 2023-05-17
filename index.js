aac7379ff55248f8bf86d30b6e14618256f27930
BhupeshB7 <96219279+BhupeshB7@users.noreply.github.com>
2023-05-18 00:59:30 +0530

    update

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ mongoose.connect(process.env.MONGO_URL, {
     .catch((error) => console.error(error));
   // Middleware
   app.use(cors({
-    // origin:"http://localhost:3000"
-    origin:"https://globalsuccesspoint.netlify.app"
+    origin:"http://localhost:3000"
+    // origin:"https://globalsuccesspoint.netlify.app"
     // origin:"https://globalsuccesspoint.in"
   }));
   app.use(express.json());
