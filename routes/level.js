be3ca3a0d2bc7c53b929ffc50c5270a25e95797a
BhupeshB7 <96219279+BhupeshB7@users.noreply.github.com>
2023-05-28 03:14:59 +0530

    update

--- routes/level.js
+++ routes/level.js
@@ -138,12 +138,12 @@ router.post("/updateWallet/:userId", async (req, res) => {
 //   res.json(dailyIncomeCount);
 // });
 
-function getDateString(date) {
-  const year = date.getFullYear();
-  const month = String(date.getMonth() + 1).padStart(2, '0');
-  const day = String(date.getDate()).padStart(2, '0');
-  return `${year}-${month}-${day}`;
-}
+// function getDateString(date) {
+//   const year = date.getFullYear();
+//   const month = String(date.getMonth() + 1).padStart(2, '0');
+//   const day = String(date.getDate()).padStart(2, '0');
+//   return `${year}-${month}-${day}`;
+// }
 
 
 
