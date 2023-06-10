54930c2a43e783e567c289f412fa929088821b19
BhupeshB7 <96219279+BhupeshB7@users.noreply.github.com>
2023-06-10 23:18:12 +0530

    update

--- routes/Admin/admin.js
+++ routes/Admin/admin.js
@@ -81,7 +81,20 @@ router.delete('/api/users/:id', async (req, res) => {
 
   res.json({ message: 'User deleted successfully' });
 });
+router.delete('/api/deposit/:id', async (req, res) => {
+  const { id } = req.params;
 
+  const user = await User.findById(id);
+
+  if (!user) {
+    return res.status(404).json({ message: 'User not found' });
+  }
+
+
+  await User.findByIdAndDelete(id);
+
+  res.json({ message: 'User deleted successfully' });
+});
 
 const isActive = (item) => item.is_active;
 
