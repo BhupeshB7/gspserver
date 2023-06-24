11d0a7d28d0972af4a745a03c6d3f5c308acbae1
BhupeshB7 <96219279+BhupeshB7@users.noreply.github.com>
2023-06-24 11:35:54 +0530

    update

--- routes/auth.js
+++ routes/auth.js
@@ -38,7 +38,7 @@ router.post("/login", async (req, res) => {
       },
     };
     const token = jwt.sign(payload, process.env.JWT_SECRET, {
-      expiresIn: "24h",
+      expiresIn: "1h",
     });
     res.json({ token });
     // }else{
